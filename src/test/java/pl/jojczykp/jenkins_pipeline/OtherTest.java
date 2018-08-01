17def1f76a5c73b32901043c8033dc4ed3b59822
jojczykp <pawel.jojczyk@gmail.com>
2018-08-01 01:28:13 +0100

    More fancy sample test

--- src/test/java/pl/jojczykp/jenkins_pipeline/OtherTest.java
+++ src/test/java/pl/jojczykp/jenkins_pipeline/OtherTest.java
@@ -8,14 +8,36 @@ class OtherTest {
 
     @Test
     void shouldPrintAndPass() {
-        System.out.println("Print to stdout");
-        System.err.println("Print to stderr");
+        System.out.println("========");
+        System.out.println(" Step 1 ");
+        System.out.println("========");
+        System.out.println("--------");
+        System.out.println("Request");
+        System.out.println("--------");
+        System.out.println("Header1: abc");
+        System.out.println("Header2: xyz");
+        System.out.println("");
+        System.out.println("{");
+        System.out.println("   \"json\": \"body\"");
+        System.out.println("}");
+        System.out.println("--------");
+        System.out.println("Response");
+        System.out.println("--------");
+        System.out.println("Header1: abc");
+        System.out.println("Header2: xyz");
+        System.out.println("");
+        System.out.println("{");
+        System.out.println("   \"json\": \"body\"");
+        System.out.println("}");
+
+        System.err.println("Some Error Log Message");
+
+        fail("Oooopppsss.... Failing :)");
     }
 
     @Test
     void shouldPrintAndFail() {
-        System.out.println("Print to stdout");
-        System.err.println("Print to stderr");
-        fail("Oooopppsss....");
+        System.out.println("Some StdOut Log Message");
+        System.err.println("Some StdErr Log Message");
     }
 }
